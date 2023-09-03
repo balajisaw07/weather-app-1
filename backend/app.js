cf3ac5448e88ff8ccba130d92a5315b805ef67bf
Davitt Barry <davittbarry333@gmail.com>
2023-09-03 11:21:34 +0300

    Adding missing packages, configuring ports and removing extra .env files

--- backend/app.js
+++ backend/app.js
@@ -26,8 +26,7 @@ mongoose
 app.use('/', indexRoutes);
 app.use('/user', userRoutes);
 
-const PORT = 3000;
-
+const PORT = process.env.BACKEND_PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
