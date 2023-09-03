48fe55c9b2912484240c8f67240cceb9ed1c4bfd
Davitt Barry <davittbarry333@gmail.com>
2023-09-03 11:54:59 +0300

    Refactoring ports and setting up backend for individual backend repo

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ mongoose
 app.use('/', indexRoutes);
 app.use('/user', userRoutes);
 
-const PORT = process.env.BACKEND_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
