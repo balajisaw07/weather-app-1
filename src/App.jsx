1a1ee2ca4041af58861702e6321f58362236d8c2
Davitt Barry <davittbarry333@gmail.com>
2023-09-07 13:43:38 +0300

    Fixing footer to bottom of page, and small styling changes to weather display

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import AppRouter from './AppRouter';
 import Navigation from './components/Navigation';
 import { DefaultCountryProvider } from './contexts/DefaultCountryContext';
 import Footer from './components/Footer';
+import './styles/global.scss';
 
 function App() {
     return (
         <DefaultCountryProvider>
-            {' '}
-            <div className="App">
-                <AppRouter>
-                    <Navigation />
-                </AppRouter>
+            <div className="App" style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+                <div style={{ flex: '1' }}>
+                    <AppRouter>
+                        <Navigation />
+                    </AppRouter>
+                </div>
                 <Footer />
             </div>
         </DefaultCountryProvider>
